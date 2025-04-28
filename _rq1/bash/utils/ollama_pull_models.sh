b07b26d7faa7acc4c0f860df2f984c0c2d526110
davide <davide.molinelli@mail.polimi.it>
2025-04-28 18:11:02 +0200

    [RQ1]
    Fixed wrong paths and variables name in ollama script

--- _rq1/bash/utils/ollama_pull_models.sh
+++ _rq1/bash/utils/ollama_pull_models.sh
@@ -4,10 +4,10 @@
 # Get current directory
 current_dir=$(realpath "$(dirname "${BASH_SOURCE[@]}")")
 # Setup global variables
-source "${current_dir}/../../../scripts/utils/global_variables.sh"
+source "${current_dir}/../../../scripts/utils/bash/global_variables.sh"
 
 # Download the models
 while IFS=, read -r ol_model _ _; do
   echo "Pulling model ${ol_model}"
   ollama pull "${ol_model}"
-done < "$RQ1_MODELS_LIST"
+done < "$VANILLA_LLMS_LIST"
