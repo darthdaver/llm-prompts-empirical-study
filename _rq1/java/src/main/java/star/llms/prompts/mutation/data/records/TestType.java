7a15aa6743e72789cc6b2f3ef94241a5f8035f97
davide <davide.molinelli@mail.polimi.it>
2025-05-18 17:25:04 +0200

    [MUTATION]
    Forgot to add enum TestType. Added.

---
+++ _rq1/java/src/main/java/star/llms/prompts/mutation/data/records/TestType.java
@@ -0,0 +1,22 @@
+package star.llms.prompts.mutation.data.records;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum TestType {
+    INFERENCE("inference"),
+    NO_ORACLE("no_oracle"),
+    PROTECTED("protected");
+
+    private final String testType;
+
+    TestType(String testType) {
+        this.testType = testType;
+    }
+
+    public String getTestType() {
+        return testType;
+    }
+
+}
